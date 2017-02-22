aa147239ffd08db68ae651c43a95df3dbed01d53
Per Andersen <perandersen@dark-cosmology.dk>
2017-02-22 11:06:58 +0100

    Added additionalplots

--- Visualisation.py
+++ Visualisation.py
@@ -148,8 +148,13 @@ def oplot_deltamus(chain_name, bins, smoothings, tolerance=0.005, label='CPL', t
     if ignore_k:
         plt.plot(redshifts, deltamu[:,0],label=label)
     else:
-        plt.plot(redshifts, deltamu_max_global,'k',ls='--',lw=4,label=label)
-        plt.plot(redshifts, deltamu_min_global,'k',ls='--',lw=4)
+        #plt.plot(redshifts, deltamu_max_global,'k',ls='--',lw=4,label=label)
+        #plt.plot(redshifts, deltamu_min_global,'k',ls='--',lw=4)
+        dashes = [20,5]
+        lmax,=plt.plot(redshifts, deltamu_max_global,'k',ls='--',lw=3,label=label)
+        lmin,=plt.plot(redshifts, deltamu_min_global,'k',ls='--',lw=3)
+        lmax.set_dashes(dashes)
+        lmin.set_dashes(dashes)
 
     plt.legend(frameon=False)
 
@@ -186,7 +191,8 @@ def oplot_deltamu_test(chain_name,bins, smoothings, tolerance=0.005, label='CPL'
     plt.fill_between(redshifts, deltamu_max_global,deltamu_min_global,color='b',label=label)
     plt.fill_between(redshifts_test, deltamu_max_global_test,deltamu_min_global_test,color='g',hatch='X',label=label + r", $w_0=-1, w_a=0$")
     plt.legend(frameon=False)
-    plt.savefig('Figures/test.pdf',format='pdf',dpi=fig.dpi)
+    if individual_plots:
+        plt.savefig('Figures/test.pdf',format='pdf',dpi=fig.dpi)
     #plt.plot(redshifts, deltamu_max_global,c='b')
     #plt.plot(redshifts, deltamu_min_global,c='b')
     #plt.plot(redshifts_test, deltamu_max_global_test,c='g')
@@ -292,6 +298,10 @@ def plot_equation_of_state(wa_1,wa_2):
     plt.legend(frameon=False, loc=2, fontsize=17)
     plt.xticks(size='x-large')
     plt.yticks(size='x-large')
+    #plt.text(7.5,-0.8,'Thawing\n    CPL',size='xx-large',color='b')
+    #plt.text(6,-0.6,'Freezing\n  n7CPL',size='xx-large',color='g')
+    plt.text(7.5,-0.8,'Thawing',size='xx-large',color='b')
+    plt.text(6,-0.6,'Freezing',size='xx-large',color='g')
     fig.set_tight_layout('True')
     plt.savefig('Figures/equationofstate.pdf',format='pdf')
 
@@ -299,7 +309,7 @@ def combined_plot():
     f, (ax1, ax2, ax3) = plt.subplots(3,2,figsize=(8,10))
     
     plt.sca(ax1[0])
-    oplot_deltamus('cpl', [(30,30,30),(40,40,40),(50,50,50)],[0.6],label='CPL',ignore_k=True,thinning=20)
+    oplot_deltamus('cpl', [(30,30,30),(40,40,40),(50,50,50)],[0.6],label='CPL',ignore_k=True,thinning=30)
     ax1[0].set_ylabel(r'$\mathbf{\Delta \mu}$',size='x-large')
     ax1[0].set_yticks([-0.08, -0.04, 0., 0.04, 0.08])
     ax1[0].set_xticks([0.])
@@ -315,7 +325,7 @@ def combined_plot():
     ax1[1].text(0.3,0.08,'(b)',size='x-large')
 
     plt.sca(ax2[0])
-    oplot_deltamus('cpl', [(30,30,30),(40,40,40),(50,50,50)],[0.6],label='CPL',ignore_k=False,thinning=40)
+    oplot_deltamus('cpl', [(30,30,30),(40,40,40),(50,50,50)],[0.6],label='CPL',ignore_k=False,thinning=50)
     ax2[0].set_ylabel(r'$\mathbf{\Delta \mu}$',size='x-large')
     ax2[0].set_yticks([-0.08, -0.04, 0., 0.04, 0.08])
     ax2[0].set_xticks([0.])
@@ -323,7 +333,7 @@ def combined_plot():
     ax2[0].text(0.3,0.08,'(c)',size='x-large')
 
     plt.sca(ax2[1])
-    oplot_deltamus('jbp', [(30,30,30),(40,40,40),(50,50,50)],[0.6],label='JBP',ignore_k=False,thinning=40)
+    oplot_deltamus('jbp', [(30,30,30),(40,40,40),(50,50,50)],[0.6],label='JBP',ignore_k=False,thinning=50)
     ax2[1].set_yticks([0.])
     ax2[1].set_yticklabels([''])
     ax2[1].set_xticks([0.])
@@ -331,7 +341,7 @@ def combined_plot():
     ax2[1].text(0.3,0.08,'(d)',size='x-large')
 
     plt.sca(ax3[0])
-    oplot_deltamus('n3cpl', [(30,30,30),(40,40,40),(50,50,50)],[0.6],label='n3CPL',ignore_k=False,thinning=40)
+    oplot_deltamus('n3cpl', [(30,30,30),(40,40,40),(50,50,50)],[0.6],label='n3CPL',ignore_k=False,thinning=50)
     ax3[0].set_ylabel(r'$\mathbf{\Delta \mu}$',size='x-large')
     ax3[0].set_yticks([-0.08, -0.04, 0., 0.04, 0.08])
     ax3[0].set_xlabel('Redshift',size='x-large')
@@ -339,7 +349,7 @@ def combined_plot():
 
 
     plt.sca(ax3[1])
-    oplot_deltamus('n7cpl', [(30,30,30),(40,40,40)],[0.4],label='n7CPL',ignore_k=False,thinning=40)
+    oplot_deltamus('n7cpl', [(30,30,30),(40,40,40)],[0.4],label='n7CPL',ignore_k=False,thinning=50)
     ax3[1].set_xlabel('Redshift',size='x-large')
     ax3[1].set_yticks([0.])
     ax3[1].set_yticklabels([''])
@@ -349,6 +359,61 @@ def combined_plot():
 
     plt.subplots_adjust(left=0.11,bottom=0.05,right=0.98,top=0.98,wspace=0., hspace=0.)
     plt.savefig('Figures/combinedplot.pdf',format='pdf')
+
+def additional_plots():
+    f, (ax1, ax2, ax3) = plt.subplots(3,2,figsize=(8,10))
+    
+    plt.sca(ax1[0])
+    oplot_deltamu_test('jbp', [(30,30,30),(40,40,40),(50,50,50)],[0.6],label='JBP')
+    ax1[0].set_ylabel(r'$\mathbf{\Delta \mu}$',size='x-large')
+    ax1[0].set_yticks([-0.08, -0.04, 0., 0.04, 0.08])
+    ax1[0].set_xticks([0.])
+    ax1[0].set_xticklabels([''])
+    ax1[0].text(0.3,0.08,'(a)',size='x-large')
+    
+    plt.sca(ax1[1])
+    oplot_deltamu_test('n3cpl', [(30,30,30),(40,40,40),(50,50,50)],[0.6],label='n3CPL')
+    ax1[1].set_yticks([0.])
+    ax1[1].set_yticklabels([''])
+    ax1[1].set_xticks([0.])
+    ax1[1].set_xticklabels([''])
+    ax1[1].text(0.3,0.08,'(b)',size='x-large')
+
+    plt.sca(ax2[0])
+    oplot_deltamu_test('n7cpl', [(30,30,30),(40,40,40)],[0.4],label='n7CPL')
+    ax2[0].set_ylabel(r'$\mathbf{\Delta \mu}$',size='x-large')
+    ax2[0].set_yticks([-0.08, -0.04, 0., 0.04, 0.08])
+    ax2[0].set_xticks([0.])
+    ax2[0].set_xticklabels([''])
+    ax2[0].text(0.3,0.08,'(c)',size='x-large')
+
+    plt.sca(ax2[1])
+    oplot_deltamus('jbp', [(30,30,30),(40,40,40),(50,50,50)],[0.6],label='JBP',ignore_k=True,thinning=30)
+    ax2[1].set_yticks([0.])
+    ax2[1].set_yticklabels([''])
+    ax2[1].set_xticks([0.])
+    ax2[1].set_xticklabels([''])
+    ax2[1].text(0.3,0.08,'(d)',size='x-large')
+
+    plt.sca(ax3[0])
+    oplot_deltamus('n3cpl', [(30,30,30),(40,40,40),(50,50,50)],[0.6],label='n3CPL',ignore_k=True,thinning=30)
+    ax3[0].set_ylabel(r'$\mathbf{\Delta \mu}$',size='x-large')
+    ax3[0].set_yticks([-0.08, -0.04, 0., 0.04, 0.08])
+    ax3[0].set_xlabel('Redshift',size='x-large')
+    ax3[0].text(0.3,0.08,'(e)',size='x-large')
+
+
+    plt.sca(ax3[1])
+    oplot_deltamus('n7cpl', [(30,30,30),(40,40,40)],[0.4],label='n7CPL',ignore_k=True,thinning=30)
+    ax3[1].set_xlabel('Redshift',size='x-large')
+    ax3[1].set_yticks([0.])
+    ax3[1].set_yticklabels([''])
+    ax3[1].set_xticks([2,4,6,8,10])
+    ax3[1].text(0.3,0.08,'(f)',size='x-large')
+
+
+    plt.subplots_adjust(left=0.11,bottom=0.05,right=0.98,top=0.98,wspace=0., hspace=0.)
+    plt.savefig('Figures/additionalplots.pdf',format='pdf')
 
 def oplot_deltamu_extrema(chain_names, bins_list, smoothings_list, labels, tolerance=0.005):
     if individual_plots:
@@ -392,8 +457,9 @@ def oplot_deltamu_extrema(chain_names, bins_list, smoothings_list, labels, toler
 
 root_dir = '/Users/perandersen/Data/HzSC/Deltamu/'
 
-individual_plots = True
+individual_plots = False
 #combined_plot()
+additional_plots()
 
 
 deltamu_cpl = Deltamu.Deltamu('cpl','',do_marg=True,bins_tuple=(50,50,50),smoothing=0.6)
@@ -412,12 +478,12 @@ cpl_marg_fname = deltamu_cpl.get_marg_file_name()
 #oplot_deltamus('lcdm', [70,80,90,100],[0.6],tolerance=0.01)
 
 #plt.sca(ax1[1])
-oplot_deltamu_test('n7cpl', [(30,30,30),(40,40,40)],[0.4],label='n7CPL')
+#oplot_deltamu_test('n7cpl', [(30,30,30),(40,40,40)],[0.4],label='n7CPL')
 #oplot_deltamu_test('n3cpl', [(30,30,30),(40,40,40),(50,50,50)],[0.3],label='n3CPL')
 #oplot_deltamu_test('jbp', [(30,30,30),(40,40,40),(50,50,50)],[0.6],label='JBP')
 #oplot_deltamu_test('cpl', [(30,30,30),(40,40,40),(50,50,50)],[0.6],label='CPL')
 
-#plot_equation_of_state([(-1.,0.1), (-1.,0.2), (-1.,0.3)],[(-1.,0.8), (-1.,0.9), (-1.,1.)])
-plt.show()
+#plot_equation_of_state([(-1.,0.1), (-1.,0.2), (-1.,0.3)],[(-1.,0.7), (-1.,0.8), (-1.,.9)])
+#plt.show()
 
 
